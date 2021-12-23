66f017f428debac188f69929d071f5bc7c5d6821
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-12-23 20:16:33 +0530

    added sample webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mobileos=["Android","Blackberry","iPhone","Windows Phone"];
+  const manufacturer=["Samsung","HTC","Micromax","Apple"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mobile Operating System</h1>
+      <ul>
+      {
+        mobileos.map((el) => (
+          <li>{el}</li>
+        ))
+      }
+      </ul>
+      <h1>Mobile Manufacturer</h1>
+      <ul>
+      {
+        manufacturer.map((el) => (
+          <li>{el}</li>
+        ))
+      }
+      </ul>
     </div>
   );
 }
